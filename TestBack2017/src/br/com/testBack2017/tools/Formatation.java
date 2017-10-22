a2d0126c421ea288d3ba0df14df670ed27027862
Murilo Medeiros <murilo_medeiros98@hotmail.com>
2017-10-22 21:04:52 -0200

    Corrigindo a organização do projeto.

--- TestBack2017/src/br/com/testBack2017/tools/Formatation.java
+++ TestBack2017/src/br/com/testBack2017/tools/Formatation.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.testBack2017;
+package br.com.testBack2017.tools;
 
 import java.text.Normalizer;
 
